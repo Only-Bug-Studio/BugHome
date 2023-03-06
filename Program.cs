7f22c94895e3bfc9e564f3b0d61d5d9475683a70
hidiyou <hidiyou@163.com>
2023-03-06 20:45:16 +0800

    编写日志系统和设置系统

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static Setting setting = new Setting();
+    static Logger logger = new Logger("Main");
+    static void Main(string[] args)
+    {
+        logger.Log(LogLevel.Info,"Loading");
+        setting.Load();
+        logger.Log(LogLevel.Info,Translate.GetTranslate("main.welcome"));
+        logger.Log(LogLevel.Info,Translate.GetTranslate("main.starting"));
+    }
+}
---
+++ Setting.cs
@@ -0,0 +1,54 @@
+using System.Dynamic;
+class Setting
+{
+    Dictionary<string, string> settings = new Dictionary<string, string>();
+    Logger logger = new Logger("Setting");
+    public Setting()
+    {
+        settings["language"]="zh-cn";
+    }
+    public void SetValue(string key,string value)
+    {
+        settings[key]=value;
+    }
+    public string GetValue(string key)
+    {
+        string a;
+        if(settings.TryGetValue(key,out a))
+        {
+            return a;
+        }
+        logger.Log(LogLevel.Error,key+" can't find");
+        return "";
+    }
+    public void Save()
+    {
+        FileStream fs = new FileStream("setting.txt", FileMode.Create, FileAccess.Write, FileShare.None);
+        StreamWriter sw = new StreamWriter(fs);
+        foreach (var item in settings)
+        {
+            sw.WriteLine(item.Key+"="+item.Value);
+        }
+        sw.Close();
+        fs.Close();
+    }
+    public void Load()
+    {
+        try
+        {
+            string[] lines = System.IO.File.ReadAllLines("setting.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(new char[] { '=' }, 2);
+                settings[parts[0]]=parts[1];
+            }
+        }
+        catch(Exception e)
+        {
+            logger.Log(LogLevel.Warn,"Can't find setting file");
+            Save();
+        }
+        Translate.LoadTranslate(settings["language"]);
+        logger.Log(LogLevel.Info,Translate.GetTranslate("setting.load.OK"));
+    }
+}
